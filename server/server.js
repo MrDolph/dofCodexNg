b65e47eb2652d667d911c81f50191c86b4e2ae66
Dawodu Olalekan <50565849+MrDolph@users.noreply.github.com>
2025-06-07 15:21:53 +0100

    Update server.js
    Your server listens on port 5000, but Render does not use hardcoded ports. Instead, Render sets its own PORT environment variable, which your app must use.

--- server/server.js
+++ server/server.js
@@ -45,4 +45,5 @@ const configuration = new Configuration({
     }
  })
 
- app.listen(5000, () => console.log('Server is running on port http://localhost:5000'))
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server is running on port http://localhost:${PORT}`));
